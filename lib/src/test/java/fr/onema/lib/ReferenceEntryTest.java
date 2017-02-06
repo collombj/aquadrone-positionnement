922c0b7894b52591a96f0094d2ef9bef72129395
jerome <jerome.goasdoue@gmail.com>
2017-02-06 14:56:59 +0100

    Tests + implémentation +documentation classe ReferenceEntry

--- lib/src/test/java/fr/onema/lib/ReferenceEntryTest.java
+++ lib/src/test/java/fr/onema/lib/ReferenceEntryTest.java
@@ -3,9 +3,11 @@ package fr.onema.lib;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class ReferenceEntryTest {
-    private ReferenceEntry entry = new ReferenceEntry(1,2,3,4,5,6);
+    /**Positive Parameter Values**/
+    private static ReferenceEntry entry = new ReferenceEntry(1,2,3,4,5,6);
 
     @Test
     public void testGetTemperature() {
@@ -35,5 +37,43 @@ public class ReferenceEntryTest {
     @Test
     public void testGetTimestamp() {
         assertEquals(entry.getTimestamp(), 1);
+    }
+
+    @Test
+    public void testToCSV() {
+        assertEquals(entry.toCSV(), "1,2,3,4,5,6");
+    }
+
+    /**Negative Parameter Values**/
+    private static ReferenceEntry entryNegative = new ReferenceEntry(1,-2,-3,-4,5,-6);
+
+    @Test
+    public void testGetNegativeTemperature() {
+        assertEquals(entryNegative.getTemperature(), -6);
+    }
+
+    @Test
+    public void testGetNegativeAlt() {
+        assertEquals(entryNegative.getAlt(), -4);
+    }
+
+    @Test
+    public void testGetNegativeLon() {
+        assertEquals(entryNegative.getLon(), -3);
+    }
+
+    @Test
+    public void testGetNegativeLat() {
+        assertEquals(entryNegative.getLat(), -2);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testDeclarationNegativeTimestamp(){
+        ReferenceEntry entryNegative = new ReferenceEntry(-1,-2,-3,-4,5,-6);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testDeclarationNegativeDirection(){
+        ReferenceEntry entryNegative = new ReferenceEntry(1,-2,-3,-4,-5,-6);
     }
 }
