14a27a698bc6283c332f324600275191b50854a8
You Thibault <thibaultyou@gmail.com>
2017-02-07 12:26:14 +0100

    Fix tests RefEntry / VirtEntry (Théo doit modifier ces classes)

--- lib/src/test/java/fr/onema/lib/virtualizer/entry/ReferenceEntryTest.java
+++ lib/src/test/java/fr/onema/lib/virtualizer/entry/ReferenceEntryTest.java
@@ -1,8 +1,87 @@
 package fr.onema.lib.virtualizer.entry;
 
-/**
- * Created by Theo on 06/02/2017.
- */
-public class ReferenceEntryTest {
+import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
+public class ReferenceEntryTest {
+    /**
+     * Positive Parameter Values
+     **/
+    private static ReferenceEntry entry = new ReferenceEntry(1, 2, 3, 4, 5, (short)6);
+    /**
+     * Negative Parameter Values
+     **/
+    private static ReferenceEntry entryNegative = new ReferenceEntry(1, -2, -3, -4, 5, (short)-6);
+
+    @Test
+    public void testGetTemperature() {
+        assertEquals(entry.getTemperature(), 6);
+    }
+/*
+    @Test
+    public void testGetDirection() {
+        assertEquals(entry.getDirection(), 5);
+    }
+*/
+    @Test
+    public void testGetAlt() {
+        assertEquals(entry.getAlt(), 4);
+    }
+
+    @Test
+    public void testGetLon() {
+        assertEquals(entry.getLon(), 3);
+    }
+
+    @Test
+    public void testGetLat() {
+        assertEquals(entry.getLat(), 2);
+    }
+
+    @Test
+    public void testGetTimestamp() {
+        assertEquals(entry.getTimestamp(), 1);
+    }
+/*
+    @Test
+    public void testToCSV() {
+        assertEquals(entry.toCSV(), "1,2,3,4,5,6");
+    }
+*/
+/*
+    @Test
+    public void testCSVHeader() {
+        assertEquals(entry.getCSVHeader(), "timestamp, latitude, longitude, altitude, direction, temperature");
+    }
+*/
+    @Test
+    public void testGetNegativeTemperature() {
+        assertEquals(entryNegative.getTemperature(), -6);
+    }
+
+    @Test
+    public void testGetNegativeAlt() {
+        assertEquals(entryNegative.getAlt(), -4);
+    }
+
+    @Test
+    public void testGetNegativeLon() {
+        assertEquals(entryNegative.getLon(), -3);
+    }
+
+    @Test
+    public void testGetNegativeLat() {
+        assertEquals(entryNegative.getLat(), -2);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testDeclarationNegativeTimestamp() {
+        ReferenceEntry entryNegative = new ReferenceEntry(-1, -2, -3, -4, 5, (short)-6);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testDeclarationNegativeDirection() {
+        ReferenceEntry entryNegative = new ReferenceEntry(1, -2, -3, -4, -5, (short)-6);
+    }
 }
