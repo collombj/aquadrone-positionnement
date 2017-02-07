b78bc243a64e6929a1b60e686747bdb7a8722c20
Nope-Nope-Nope\Acer <francois.vanderperre@gmail.com>
2017-02-07 16:10:40 +0100

    48_Developpement DatabaseDriver
    implementation des methodes

--- lib/src/main/java/fr/onema/lib/database/DatabaseDriver.java
+++ lib/src/main/java/fr/onema/lib/database/DatabaseDriver.java
@@ -1,11 +1,13 @@
 package fr.onema.lib.database;
 
 import fr.onema.lib.database.entity.DiveEntity;
-import fr.onema.lib.drone.Measure;
+import fr.onema.lib.database.entity.MeasureEntity;
+import fr.onema.lib.geo.GPSCoordinate;
 
-import java.sql.Connection;
-import java.util.Date;
-import java.util.stream.Stream;
+import java.sql.*;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Properties;
 
 /**
  * Created by Francois Vanderperre on 07/02/2017.
@@ -18,39 +20,119 @@ public class DatabaseDriver {
     private String base;
     private String user;
     private String password;
+    private String srid;
     private Connection connector;
 
-    private DatabaseDriver(String host, int port, String base, String user, String password) {
+    private DatabaseDriver(String host, int port, String base, String user, String srid, String password) {
         this.host = host;
         this.port = port;
         this.base = base;
         this.user = user;
         this.password = password;
+        this.srid = srid;
     }
 
-    public class DatabaseDriverFactory {
-        public DatabaseDriver getDatabaseDriver() { //TODO doit recevoir une configuration en argument
+    public class DatabaseDriverFactory {//TODO doit recevoir une configuration en argument
+
+        public DatabaseDriver getDatabaseDriver() {
             return null;
         }
     }
 
-    public void initAsReadable() {//TODO
-
+    public void initAsReadable() {
+        Properties props = new Properties();
+        props.setProperty("readOnly", "true");
+        initConnection(props);
     }
 
-    public void initAsWritable() {//TODO
-
+    public void initAsWritable() {
+        Properties props = new Properties();
+        props.setProperty("readOnly", "false");
+        initConnection(props);
     }
 
-    public Stream<Measure> getMeasureFrom(DiveEntity dive) {//TODO
+    private void initConnection(Properties props) {
+        try {
+            Class.forName("org.postgresql.Driver");
+            String url = "jdbc:postgresql://" + host + ":" + port + "/" + base;
+            props.setProperty("user", user);
+            props.setProperty("password", password);
+            connector = DriverManager.getConnection(url, props);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Postgresql driver not found");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Failed to connect to database");
+            e.printStackTrace();
+        }
+    }
+
+    public void closeConnection() {
+        try {
+            connector.close();
+        } catch (SQLException e) {
+            System.out.println("Failed to close connection to database");
+            e.printStackTrace();
+        }
+    }
+
+    public List<MeasureEntity> getMeasureFrom(DiveEntity dive) {
+        List<MeasureEntity> mesures = new LinkedList<>();
+        try (PreparedStatement ps = connector.prepareStatement("SELECT id, timestamp, ST_X(location_brut) as brutX," +
+                "ST_Y(location_brut) as brutY, ST_Z(location_brut) as brutZ, ST_X(location_corrected) as correctX," +
+                "ST_Y(location_corrected) as correctY, ST_Z(location_corrected) as correctZ, accelerationX," +
+                " accelerationY, accelerationZ, rotationX, rotationY, rotationZ, precision_cm, measure_value" +
+                "  FROM Measure WHERE #dive_id=? ORDER BY id")) {
+            ps.setInt(1, dive.getId());
+            ResultSet results = ps.executeQuery();
+            while (results.next()) {
+                int id = Integer.parseInt(results.getString("id"));
+                long timestamp = Long.parseLong(results.getString("timestamp"));
+                long brutX = Long.parseLong(results.getString("brutX"));
+                long brutY = Long.parseLong(results.getString("brutY"));
+                long brutZ = Long.parseLong(results.getString("brutZ"));
+                long correctX = Long.parseLong(results.getString("correctX"));
+                long correctY = Long.parseLong(results.getString("correctY"));
+                long correctZ = Long.parseLong(results.getString("correctZ"));
+
+                int accelerationX = Integer.parseInt(results.getString("accelerationX"));
+                int accelerationY = Integer.parseInt(results.getString("accelerationY"));
+                int accelerationZ = Integer.parseInt(results.getString("accelerationZ"));
+                int rotationX = Integer.parseInt(results.getString("rotationX"));
+                int rotationY = Integer.parseInt(results.getString("rotationY"));
+                int rotationZ = Integer.parseInt(results.getString("rotationZ"));
+
+                int precisionCm = Integer.parseInt(results.getString("precision_cm"));
+                String measureValue = results.getString("measure_value");
+                mesures.add(new MeasureEntity(id, timestamp, new GPSCoordinate(brutX, brutY, brutZ),
+                        new GPSCoordinate(correctX, correctY, correctZ), accelerationX, accelerationY, accelerationZ,
+                        rotationX, rotationY, rotationZ, precisionCm, measureValue));
+
+            }
+        } catch (SQLException e) {
+            System.out.println("Failed to retrieve the last dive");
+            e.printStackTrace();
+        }
+        return mesures;
+    }
+
+    public DiveEntity getLastDive() {
+        try (PreparedStatement ps = connector.prepareStatement("SELECT * FROM Dive ORDER BY id DESC LIMIT 1")) {
+            ResultSet results = ps.executeQuery();
+            if (results.next()) {
+                int id = Integer.parseInt(results.getString("id"));
+                long start = Long.parseLong(results.getString("start_time"));
+                long end = Long.parseLong(results.getString("end_time"));
+                return new DiveEntity(id, start, end);
+            }
+        } catch (SQLException e) {
+            System.out.println("Failed to retrieve the last dive");
+            e.printStackTrace();
+        }
         return null;
     }
 
-    public DiveEntity getLastDive() {//TODO
-        return null;
-    }
-
-    public int insertDive(Date date) {//TODO
+    public int insertDive(long dateStart, long dateEnd) {//TODO
         return 0;
     }
 
@@ -58,16 +140,43 @@ public class DatabaseDriver {
         return 0;
     }
 
-    public void updatePosition() {//TODO changer les arguments
+    public void updatePosition(int measureId, long lat, long lon, long alt, int precision) {
+        try (PreparedStatement ps = connector.prepareStatement("UPDATE Measure SET location_corrected = " +
+                "ST_SetSRID(ST_MakePoint(?, ?, ?), ?), precision_cm = ?  WHERE id = ?")) {
+            ps.setLong(1, lon);
+            ps.setLong(2, lat);
+            ps.setLong(3, alt);
+            ps.setString(4, srid);
+            ps.setInt(5, precision);
+            ps.setInt(6, measureId);
+            ps.execute();
 
+        } catch (SQLException e) {
+            System.out.println("Failed correcting mesure n°" + measureId);
+            e.printStackTrace();
+        }
     }
 
     public void startRecording(long timestamp, int diveId) {
-
+        try (PreparedStatement ps = connector.prepareStatement("UPDATE Dive SET start_time = ? WHERE id = ?")) {
+            ps.setLong(1, timestamp);
+            ps.setLong(2, diveId);
+            ps.execute();
+        } catch (SQLException e) {
+            System.out.println("Failed updating dive n°" + diveId);
+            e.printStackTrace();
+        }
     }
 
     public void stopRecording(long timestamp, int diveId) {
-        
+        try (PreparedStatement ps = connector.prepareStatement("UPDATE Dive SET end_time = ? WHERE id = ?")) {
+            ps.setLong(1, timestamp);
+            ps.setLong(2, diveId);
+            ps.execute();
+        } catch (SQLException e) {
+            System.out.println("Failed updating dive n°" + diveId);
+            e.printStackTrace();
+        }
     }
 
 }
