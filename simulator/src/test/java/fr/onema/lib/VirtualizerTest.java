3760bac9f398dca78300aa5196e261237c7a5fb2
jerome <jerome.goasdoue@gmail.com>
2017-02-08 18:31:22 +0100

    Implémentation Virtualizer

--- simulator/src/test/java/fr/onema/lib/VirtualizerTest.java
+++ simulator/src/test/java/fr/onema/lib/VirtualizerTest.java
@@ -1,18 +1,32 @@
 package fr.onema.lib;
 
+import fr.onema.lib.file.FileManager;
 import fr.onema.simulator.Virtualizer;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Jérôme on 08/02/2017.
  */
 public class VirtualizerTest {
-    //
     @Test (expected = NullPointerException.class)
     public void testNullFileManager(){
         Virtualizer v = new Virtualizer(null, 100, "null");
         v.start();
 
+    }
+
+    @Test
+    public void testFileManager(){
+        FileManager fm = new FileManager("setting.properties","toto");
+        Virtualizer v = new Virtualizer(fm, 100, "null");
+        v.start();
+        long a = v.getDuration();
+        assertEquals(v.getDuration(),a);
+        assertEquals(v.getSimulationName(), "null");
+        assertEquals(v.getFileManager(), fm);
+        assertEquals(v.getSpeed(), 100);
     }
 
 
