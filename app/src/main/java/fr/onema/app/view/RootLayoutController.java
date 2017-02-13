2d4ae7256b932f1f2d0227c326527e41459b5846
You Thibault <thibaultyou@gmail.com>
2017-02-13 17:44:44 +0100

    #72 Layout global : gestion du pop-up configuration et son fxml, ajout des contrôles dans le layout root

--- app/src/main/java/fr/onema/app/view/RootLayoutController.java
+++ app/src/main/java/fr/onema/app/view/RootLayoutController.java
@@ -1,21 +1,36 @@
 package fr.onema.app.view;
 
-import fr.onema.app.Main;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class RootLayoutController {
-    private Main main;
-
-    public void setMain(Main main) {
-        this.main = main;
-    }
 
     @FXML
-    private Label testLabel;
+    private Button configurationButton;
 
     @FXML
-    private void testAction() {
-        testLabel.setText("Ceci est un test");
+    private void testAction(ActionEvent event) throws IOException {
+        Stage stage;
+        Stage root;
+        root = (Stage)configurationButton.getScene().getWindow();
+
+        if (event.getSource() == configurationButton) {
+            stage = new Stage();
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("ConfigurationLayout.fxml"))));
+            stage.setTitle("Configuration");
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.initOwner(root);
+            stage.showAndWait();
+        } else {
+            stage = root;
+            stage.close();
+        }
     }
 }
