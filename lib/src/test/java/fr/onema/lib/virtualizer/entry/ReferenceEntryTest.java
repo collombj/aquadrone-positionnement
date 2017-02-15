fc0e97bc3b540b5316ab7f32986452bacd95ea80
Pascoli <theo.pascoli@gmail.com>
2017-02-15 10:09:38 +0100

    82-Debug-Coverage

--- lib/src/test/java/fr/onema/lib/virtualizer/entry/ReferenceEntryTest.java
+++ lib/src/test/java/fr/onema/lib/virtualizer/entry/ReferenceEntryTest.java
@@ -29,4 +29,56 @@ public class ReferenceEntryTest {
         assertEquals("timestamp,latitude,longitude,altitude,direction,temperature", ref.getCSVHeader());
     }
 
+    @Test(expected=IllegalArgumentException.class)
+    public void constructorTimestampNeg() {
+        ReferenceEntry ref = new ReferenceEntry(-1, 1,1,1, 1, (short)1 );
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void constructorDirectionNeg() {
+        ReferenceEntry ref = new ReferenceEntry(1, 1,1,1, -4, (short)1 );
+    }
+
+    @Test
+    public void toCSVComputeTest() {
+        ReferenceEntry ref = new ReferenceEntry(1, 1,2,3, 4, (short)1 );
+        assertEquals("1,2,3,4.0", ref.toCSVforComputedFormat());
+    }
+
+    @Test
+    public void getTimestampTest() {
+        ReferenceEntry ref = new ReferenceEntry(1, 2,3,4, 5, (short)6 );
+        assertEquals(1,ref.getTimestamp());
+    }
+
+    @Test
+    public void getLatTest() {
+        ReferenceEntry ref = new ReferenceEntry(1, 2,3,4, 5, (short)6 );
+        assertEquals(2,ref.getLat());
+    }
+
+    @Test
+    public void getLonTest() {
+        ReferenceEntry ref = new ReferenceEntry(1, 2,3,4, 5, (short)6 );
+        assertEquals(3,ref.getLon());
+    }
+
+    @Test
+    public void getAltTest() {
+        ReferenceEntry ref = new ReferenceEntry(1, 2,3,4, 5, (short)6 );
+        assertEquals(4,ref.getAlt());
+    }
+
+    @Test
+    public void getDirectionTest() {
+        ReferenceEntry ref = new ReferenceEntry(1, 2,3,4, 5, (short)6 );
+        assertEquals(5.0,ref.getDirection(),0);
+    }
+
+    @Test
+    public void getTemperatureTest() {
+        ReferenceEntry ref = new ReferenceEntry(1, 2,3,4, 5, (short)6 );
+        assertEquals(6,ref.getTemperature());
+    }
+
 }
