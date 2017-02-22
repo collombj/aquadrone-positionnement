0cd97a24b450a916c81dc34f8f2f6ef698f9971b
Theo Pascoli <theo.pascoli@gmail.com>
2017-02-22 17:16:25 +0100

    103-resolution-(oui-encore-une)

--- lib/src/test/java/fr/onema/lib/network/NetworkSenderTest.java
+++ lib/src/test/java/fr/onema/lib/network/NetworkSenderTest.java
@@ -13,6 +13,7 @@ public class NetworkSenderTest {
     @Test
     public void constructorNotNull() throws IOException {
         NetworkSender networkSender = new NetworkSender(1239, "127.0.0.1");
+        networkSender.start();
         assertNotNull(networkSender);
         networkSender.closeConnection();
     }
@@ -22,6 +23,7 @@ public class NetworkSenderTest {
         NetworkSender networkSender = new NetworkSender(1239, "127.0.0.1");
         ServerSocket server = new ServerSocket(1239);
         networkSender.openConnection();
+        networkSender.start();
         assertNotNull(networkSender.getDsocket());
         networkSender.closeConnection();
         server.close();
@@ -32,6 +34,7 @@ public class NetworkSenderTest {
         NetworkSender networkSender = new NetworkSender(1239, "127.0.0.1");
         ServerSocket server = new ServerSocket(1239);
         networkSender.openConnection();
+        networkSender.start();
         networkSender.closeConnection();
         assertFalse(networkSender.getDsocket().isConnected());
         server.close();
@@ -41,6 +44,7 @@ public class NetworkSenderTest {
     public void threadStarted() throws IOException, InterruptedException {
         NetworkSender networkSender = new NetworkSender(1239, "127.0.0.1");
         networkSender.openConnection();
+        networkSender.start();
         Thread.sleep(100);
         assertTrue(networkSender.getSender().isAlive());
         networkSender.closeConnection();
@@ -51,6 +55,7 @@ public class NetworkSenderTest {
         NetworkSender networkSender = new NetworkSender(1239, "127.0.0.1");
         VirtualizerEntry vir = new VirtualizerEntry(1, (short) 5, (short) 6, (short) 7, (short) 8, (short) 9, (short) 10, (short) 11, (short) 12, (short) 13, 14, (short) 15);
         networkSender.openConnection();
+        networkSender.start();
         networkSender.add(vir);
         networkSender.getQueue().contains(vir);
         networkSender.closeConnection();
@@ -62,6 +67,7 @@ public class NetworkSenderTest {
         sender.openConnection();
         ServerListener serverListener = new ServerListener(1239);
         serverListener.start();
+        sender.start();
         VirtualizerEntry virtual = new VirtualizerEntry(1, 2,3,4, (short) 5000, (short) 6, (short) 7, (short) 8, (short) 9, (short) 10, (short) 11, (short) 12, (short) 13, 14, (short) 15);
         sender.add(virtual);
         sender.closeConnection();
@@ -71,6 +77,7 @@ public class NetworkSenderTest {
     @Test
     public void getPortTest() throws IOException {
         NetworkSender sender = new NetworkSender(1239, "127.0.0.1");
+        sender.start();
         assertEquals(1239,sender.getPort());
         sender.closeConnection();
     }
@@ -78,8 +85,8 @@ public class NetworkSenderTest {
     @Test
     public void getHostTest() throws IOException {
         NetworkSender sender = new NetworkSender(1239, "127.0.0.1");
+        sender.start();
         assertEquals("127.0.0.1",sender.getHost());
         sender.closeConnection();
     }
-
 }
